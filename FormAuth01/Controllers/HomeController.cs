2545a864134ef4070fd74df535b802805c10164f
shenqiangbin <shenqiangbin@163.com>
2017-01-30 10:49:04 +0800

    窗体认证

--- FormAuth01/Controllers/HomeController.cs
+++ FormAuth01/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace FormAuth01.Controllers
 {
@@ -11,6 +12,21 @@ namespace FormAuth01.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult LogOn()
+        {
+            //验证登录是否成功
+            //如果成功，则
+            var account = "admin";
+            FormsAuthentication.SetAuthCookie(account, false);            
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
         }
     }
 }
